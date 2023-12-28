6c1d44c9294edbe2bd2a837b49fc3843a0eed523
Ko1suz <rooooocer@gmail.com>
2023-12-28 20:11:27 +0300

    Update
    Ek referans sahne eklendi
    Arka plan seslerini Cekme

--- Assets/Scripts/Test/NAudio/NaudioTest.cs
+++ Assets/Scripts/Test/NAudio/NaudioTest.cs
@@ -8,34 +8,42 @@ using NAudio.Wave.SampleProviders;
 
 public class NaudioTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private WasapiLoopbackCapture waveIn;
+
     void Start()
     {
-        
+        // NAudio kaydý baþlat
+        waveIn = new WasapiLoopbackCapture();
+        waveIn.DataAvailable += OnDataAvailable;
+        waveIn.StartRecording();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        SoundListener();
+        // Her güncelleme döngüsünde ses verilerini iþleyin
+        if (waveIn != null && waveIn.CaptureState == NAudio.CoreAudioApi.CaptureState.Capturing)
+        {
+            //byte[] buffer = new byte[1024]; // Örnek: 1024 byte'lýk bir veri okuma
+            //int bytesRead = waveIn.Read(buffer, 0, buffer.Length);
+
+            //// Ses verilerini iþleyin
+            //// Örnek: Debug.Log ile ses verilerini yazdýrma
+            //Debug.Log("Ses Verisi Alýndý. Okunan Byte Sayýsý: " + bytesRead);
+        }
     }
 
-    void SoundListener()
+    void OnDataAvailable(object sender, WaveInEventArgs e)
     {
-        var waveIn = new WasapiLoopbackCapture();
-        waveIn.DataAvailable += (sender, e) =>
+        // Burada da ses verilerini iþleyebilirsiniz, ancak Update fonksiyonu bu iþi sürekli olarak yapar.
+    }
+
+    void OnApplicationQuit()
+    {
+        // Uygulama sona erdiðinde NAudio kaydýný durdur
+        if (waveIn != null)
         {
-            // Ses verilerini iþleyin
-            byte[] buffer = new byte[e.BytesRecorded];
-            Buffer.BlockCopy(e.Buffer, 0, buffer, 0, e.BytesRecorded);
-
-            // Unity içinde bu sesi kullanmak için gerekli iþlemleri gerçekleþtirin
-            // Örneðin, bu sesi bir ses nesnesine baðlamak için Unity API'lerini kullanýn.
-        };
-        waveIn.StartRecording();
-
-        Debug.Log("Ses dinleniyor. Çýkmak için bir tuþa basýn.");
-
-        //waveIn.StopRecording();
+            waveIn.StopRecording();
+            waveIn.Dispose();
+        }
     }
 }
